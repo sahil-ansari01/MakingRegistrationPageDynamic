b69f516fde47bcc3e95b7bc5a8fe7c24cd1a97f4
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-26 07:02:28 +0530

    Initial commit

---
+++ callback.js
@@ -0,0 +1,30 @@
+const posts = [
+    { title: 'Post one', body: 'This is post one'},
+    { title: 'Post two', body: 'This is post two'}
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+
+    }, 1000)
+}
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000)
+}
+
+function create4thPost(post4, callback) {
+    setTimeout(() => {
+        callback(post4, getPosts)
+    })
+}
+
+createPost({title: 'Post three', body: 'This is post three'}, getPosts)
